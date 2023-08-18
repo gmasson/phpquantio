12904cca59b25b35dd8fdae7e10f5679493ed7a2
Gabriel Masson <gmasson@users.noreply.github.com>
2023-08-18 20:44:20 -0300

    1.2

--- phpquantio.php
+++ phpquantio.php
@@ -1,263 +1,200 @@
 <?php
 /**
-* PHPQuantio 1.1
+* PHPQuantio 1.2
 * Micro biblioteca PHP com funções úteis para desenvolvimento web
 * https://github.com/gmasson/phpquantio
 * License MIT
 */
 
+# Verifica se tem session_start
+if (session_status() == PHP_SESSION_NONE) {
+	session_start();
+}
+
 # Verifica a versão do PHP
-if ( phpversion() < '7.0' )
-{
+if ( phpversion() < '7.3' ) {
 	die( "Please update PHP to a higher version" );
 }
 
 # Configura o Timezone
-if( ! ini_get( 'date.timezone' ) )
-{
-    date_default_timezone_set('GMT');
+if( ! ini_get( 'date.timezone' ) ) {
+	date_default_timezone_set('GMT');
 }
 
-/* Filters */
+# Filtro para textos
+function pq_filter($input, $type = '') {
+	$input = trim($input);
 
-# Filters - Text
-function phpq_filterText( $input, $type = '' )
-{
-	switch ( $type )
-	{
-		case 'print_html':
-			$input = htmlspecialchars( $input );
+	switch ($type) {
+		case 'html':
+			$input = htmlspecialchars($input, ENT_QUOTES | ENT_HTML5, 'UTF-8');
 			break;
-		
-		default:
-			$input = strip_tags( $input );
+
+		case 'url':
+			$input = htmlspecialchars($input, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+			$input = str_replace(" ", "_", $input);
 			break;
-	}
-	return $input;
-}
 
-# Filters - Integers
-function phpq_filterNumb( $input )
-{
-	$input = addslashes( $input );
-	$input = strip_tags( $input );
-	$input = intval( $input );
-	return $input;
-}
-
-# Filters - URLs
-function phpq_filterUrl( $input, $with_underline = '' )
-{
-	$input = phpq_filterText( $input, 'print_html' );
-	if ( $with_underline != '' )
-	{
-		$input = str_replace( " ", "_", $input );
-	}
-	return $input;
-}
-
-# Filters - Username
-function phpq_filterUser( $input, $with_underline = 'y' )
-{
-	$input = strip_tags( $input );
-	$input = phpq_filterText( $input );
-	$input = strtolower( $input );
-	if ( $with_underline != '' )
-	{
-		$input = str_replace( " ", "_", $input );
-	}
-	return $input;
-}
-
-# Filters - Email
-function phpq_filterEmail( $input )
-{
-	$input = phpq_filterUser( $input );
-	if( !preg_match( "/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/",$input ) )
-	{
-		return false;
-	}
-	else
-	{
-		return $input;
-	}
-}
-
-# Filters - Password
-function phpq_filterPass( $input, $key = 'try if you can' )
-{
-	$input = addslashes( $input );
-	$input = strip_tags( $input );
-	$input = $key . $input . $key;
-	$input = crypt( $input, '$2a$07$r$' );
-	//$input = md5( $input );
-	return "$2a$08$4" . $input . "X21_";
-}
-
-# Filters - GET parameters
-function phpq_filterGet( $input, $with_empty = '', $with_underline = '', $count = 0 )
-{
-	$input = ( isset( $_GET[ $input ] ) ) ? phpq_filterUrl( $_GET[ $input ], $with_underline ) : $with_empty;
-	if ( empty( $input ) or strlen( $input ) < $count )
-	{
-		$input = $with_empty;
-	}
-	return $input;
-}
-
-# Filters - POST parameters
-function phpq_filterPost( $input, $with_empty = '', $with_underline = '', $count = 0 )
-{
-	$input = ( isset( $_POST[ $input ] ) ) ? phpq_filterUrl( $_POST[ $input ], $with_underline  ) : $with_empty;
-	if ( empty( $input ) or strlen( $input ) < $count )
-	{
-		$input = $with_empty;
-	}
-	return $input;
-}
-
-
-/* Times */
-
-# Times - Current date
-function phpq_date( $tipe = '' )
-{
-	switch ($tipe) {
-		case 'd':
-			return date( 'd' );
+		case 'user':
+			$input = strtolower($input);
+			$input = str_replace(" ", "_", $input);
 			break;
-		
-		case 'm':
-			return date( 'm' );
-			break;
-		
-		case 'y':
-			return date( 'Y' );
-			break;
-		
-		case 'a':
-			return date( 'Y' );
-			break;
-		
-		case 'pt':
-			return date( 'd/m/Y' );
-			break;
-		
-		default:
-			return date( 'Y/m/d' );
-			break;
-	}
-}
 
-# Times - Current time
-function phpq_hour( $tipe = '' )
-{
-	switch ($tipe) {
-		case 'h':
-			return date( 'h' );
+		case 'invert':
+			$input = strrev($input);
 			break;
-		
-		case 'm':
-			return date( 'i' );
+
+		case 'email':
+			$input = strtolower($input);
+			$input = str_replace(" ", "_", $input);
+			if (filter_var($input, FILTER_VALIDATE_EMAIL)) {
+				return $input;
+			} else {
+				return false;
+			}
 			break;
-		
-		case 's':
-			return date( 's' );
+
+		case 'get':
+			$input = filter_input(INPUT_GET, $input, FILTER_SANITIZE_STRING);
+			$input = ($input !== null && $input !== false && $input !== '') ? $input : $with_empty;
+			break;
+
+		case 'post':
+			$input = filter_input(INPUT_POST, $input, FILTER_SANITIZE_STRING);
+			$input = ($input !== null && $input !== false && $input !== '') ? $input : $with_empty;
 			break;
 
 		default:
-			return date( 'H:i:s' );
+			$input = strip_tags($input);
 			break;
 	}
+
+	return $input;
 }
 
-/* Generators */
-
-# Generators - Hash
-function phpq_hash( $size = 12 )
-{
-	$ma = "ABCDEFGHIJKLMNOPQRSTUVYXWZ"; // Letras maiúsculas
-	$mi = "abcdefghijklmnopqrstuvyxwz"; // Letras minusculas
-	$nu = "0123456789"; // Números
-	$si = "!@#$%-&*()_+="; // Símbolos
-	
-	$pass .= str_shuffle( $ma );
-	$pass .= str_shuffle( $mi );
-	$pass .= str_shuffle( $nu );
-	$pass .= str_shuffle( $si );
-	return substr( str_shuffle( $pass ), 0, $size );
+# Gerador de senhas
+function pq_pass($input, $key = 'try +', $substr = -1) {
+	$input = pq_filter($input);
+	$input = substr($input, 0, $substr);
+	$input = strrev($input);
+	$input = $input . $key;
+	$input = password_hash($input, PASSWORD_DEFAULT);
+	$input = '$2a$' . $input;
+	return $input;
 }
 
-# Generators - Loren Ipsum
-function phpq_lorem( $length = 445 ) {
-	$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".
+# Data Atual
+function pq_date($type = '') {
+	$formats = [
+		'd' => 'd',
+		'm' => 'm',
+		'y' => 'Y',
+		'a' => 'Y',
+		'pt' => 'd/m/Y',
+	];
+
+	return isset($formats[$type]) ? date($formats[$type]) : date('Y/m/d');
+}
+
+# Hora atual
+function pq_hour($type = '') {
+	$formats = [
+		'h' => 'h',
+		'm' => 'i',
+		's' => 's',
+	];
+
+	return isset($formats[$type]) ? date($formats[$type]) : date('H:i:s');
+}
+
+# Gerador de Hash
+function pq_hash($size = 12) {
+	$characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%-&*()_+=";
+	$randomString = '';
+
+	for ($i = 0; $i < $size; $i++) {
+		$randomString .= $characters[rand(0, strlen($characters) - 1)];
+	}
+
+	return $randomString;
+}
+
+# Gerador de Loren Ipsum
+function pq_lorem($length = 445) {
+	$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	$maxLength = 445;
-	$shortened = mb_substr( $text, 0, $length, 'UTF-8' );
-	if ( $length > $maxLength ) {
+
+	if ($length > $maxLength) {
 		return $text;
 	} else {
-		return $shortened;
+		return mb_substr($text, 0, $length, 'UTF-8');
 	}
 }
 
-/* Mail */
-
-# Mail - Send
-function phpq_mail( $array )
-{
-	$headers = "MIME-Version: 1.1\r\n";
+# Envio de e-mail usando Mail
+function pq_mail($email, $subject, $body, $from) {
+	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
-	$headers .= "From: " . $array['from'] . "\r\n";
-	$headers .= "Return-Path: " . $array['return'] . "\r\n";
-	$sendMail = mail( $array[ 'email' ], $array[ 'subject' ], $array[ 'body' ], $headers );
-	
-	if ( $sendMail ) 
-	{ return true; } 
-	else 
-	{ return false; }
+	$headers .= "From: " . $from . "\r\n";
+	$headers .= "Reply-To: " . $from . "\r\n";
+	$headers .= "Return-Path: " . $from . "\r\n";
+	$sendMail = mail($email, $subject, $body, $headers);
+
+	return $sendMail;
 }
 
-/* Miscellaneous */
-
-# Miscellaneous - User browser
-function phpq_browser()
-{
-	return $_SERVER[ 'HTTP_USER_AGENT' ];
+# Navegador do usuário
+function pq_browser() {
+	return $_SERVER['HTTP_USER_AGENT'];
 }
 
-# Miscellaneous - User IP
-function phpq_ip()
-{
-	if (!empty($_SERVER['HTTP_CLIENT_IP']))
-	{
+# IP do usuário
+function pq_ip() {
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 		return $_SERVER['HTTP_CLIENT_IP'];
-	}
-	// verifica se vem de um proxy
-	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-	{
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
 		return $_SERVER['HTTP_X_FORWARDED_FOR'];
-	}
-	else
-	{
+	} else {
 		return $_SERVER['REMOTE_ADDR'];
 	}
 }
 
-# Miscellaneous - List of files in a folder
-function phpq_fileListing( $folder, $tag = '<p>', $endTag = '</p>' )
-{
+# Listar arquivos de uma pasta
+function pq_fileListing($folder, $tag = '<p>', $endTag = '</p>') {
 	$dir = dir($folder);
-	while($file = $dir -> read()){
-		echo $tag . $file . $endTag;
+	while ($file = $dir->read()) {
+		echo $tag . pq_filterText($file) . $endTag;
 	}
-	$dir -> close();
+	$dir->close();
 }
 
-# Miscellaneous - JSON information with simple structure
-function phpq_extractJson( $file, $data )
-{
-	$arquivo = file_get_contents($file);
-	$json = json_decode($arquivo);
-	return $json->$data;
+function pq_captcha($name = 'c21', $class = '') {
+	$num1 = rand(0, 10);
+	$num2 = rand(0, 10);
+	$operation = (rand(0, 1) == 0) ? '+' : '-';
+
+	if ($operation === '+') {
+		$captchaText = "$num1 + $num2";
+		$captchaResult = $num1 + $num2;
+	} else {
+		$captchaText = "$num1 - $num2";
+		$captchaResult = $num1 - $num2;
+	}
+
+	$_SESSION[$name] = $captchaResult;
+
+	$captchaHTML = "<label for='$name'>$captchaText = </label>";
+	$captchaHTML .= "<input type='number' class='$class' name='$name' required>";
+
+	return $captchaHTML;
 }
 
+function pq_validCaptcha($name = 'c21') {
+	if (isset($_SESSION[$name]) && isset(pq_filterPost($name))) {
+		$submittedResult = pq_filterPost($name);
+		$correctResult = $_SESSION[$name];
+		unset($_SESSION[$name]);
+		return $submittedResult === $correctResult;
+	}
+	return false;
+}
