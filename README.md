aee2a4a45bb865904618591edf9ed5e7819520f3
Gabriel Masson <gmasson@users.noreply.github.com>
2024-03-28 17:22:03 -0300

    1.9.0

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
-# phpquantio
-Micro biblioteca PHP com funções úteis para desenvolvimento web
+# PHPQuantio 1.9.0
+PHPQuantio é uma micro biblioteca PHP com funções úteis para desenvolvimento web.
+
+## Licença
+- MIT
+
+## Requisitos
+- PHP 7.3 ou superior
+
+## Configuração padrão
+
+### Iniciando a sessão
+A sessão é automaticamente iniciada se ainda não estiver iniciada.
+
+### Verificação da Versão do PHP
+A biblioteca requer PHP 7.3 ou superior. Se o PHP estiver em uma versão inferior, uma mensagem de erro será exibida.
+
+### Configuração do Timezone
+O Timezone é configurado para GMT se não estiver configurado.
+
+## Controle de Acessos
+A biblioteca controla o acesso limitando a 60 requisições por minuto.
+
+## Funções
+
+### pq_filter(input, type, add)
+Filtra a entrada de dados de acordo com o tipo especificado.
+
+- html: Filtra conteúdo HTML.
+- url: Filtra URL.
+- user: Filtra nome de usuário.
+- email: Filtra endereço de e-mail.
+- get: Filtra valor recebido via método GET.
+- post: Filtra valor recebido via método POST.
+- pass: Filtra senha.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_filter
+$filteredInput = pq_filter('nomeinput', 'post');
+```
+
+### pq_info(opt)
+Retorna informações sobre o cliente, servidor e ambiente de execução.
+
+- ip: Endereço IP do cliente.
+- browser: Agente do navegador (user-agent) do cliente.
+- port: Porta do cliente.
+- referer: URL da página de onde o usuário veio.
+- root: Diretório raiz do documento.
+- host: Nome do host do servidor.
+- server_addr: Endereço IP do servidor.
+- server_name: Nome do servidor.
+- server_software: Software do servidor.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_info
+$clientIP = pq_info('ip');
+```
+
+### pq_mail(email, subject, body, from)
+Envia um email usando a função mail do PHP.
+
+- email: Endereço de email de destino.
+- subject: Assunto do email.
+- body: Corpo do email.
+- from: Endereço de email do remetente.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_mail
+$emailSent = pq_mail('destinatario@example.com', 'Assunto do Email', 'Corpo do Email', 'remetente@example.com');
+```
+
+### pq_captcha(name)
+Gera um captcha simples com uma operação matemática.
+
+- name: Nome do captcha para armazenamento na sessão.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_captcha
+$captchaText = pq_captcha();
+```
+
+### pq_validCaptcha(input, name)
+Valida a resposta do captcha fornecida pelo usuário.
+
+- input: Resposta do usuário ao captcha.
+- name: Nome do captcha armazenado na sessão.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_validCaptcha
+$isValidCaptcha = pq_validCaptcha($_POST['captcha']);
+```
+
+### pq_login(input, correctPass, token)
+Realiza o login com uma senha única.
+
+- input: Senha fornecida pelo usuário.
+- correctPass: Senha correta para comparação.
+- token: Token para validar sessão.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_login
+$loggedIn = pq_login($_POST['password'], $correctPassword);
+```
+
+### pq_validLogin(token)
+Verifica se o usuário está logado.
+
+- token: Token para validar sessão.
+
+#### Exemplo:
+```php
+// Exemplo de uso da função pq_validLogin
+$isValidLogin = pq_validLogin();
+```
--- phpquantio.php
+++ phpquantio.php
@@ -1,53 +1,62 @@
 <?php
 /**
-* PHPQuantio 1.5.1
+* PHPQuantio 1.9.0
 * Micro biblioteca PHP com funções úteis para desenvolvimento web
 * https://github.com/gmasson/phpquantio
 * License MIT
 */
 
-# Chave para Senhas (atualize essa chave em cada projeto)
-define('pq_key', 'sHR1gD30FG86dye2mmDFryHJ45F2FDHRqqTU');
-
-/* NÃO ALTERE A PARTIR DAQUI */
-
-# Verifica se tem session_start
+# Verifica e inicia a sessão se não estiver iniciada
 if (session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 
 # Verifica a versão do PHP
 if (version_compare(phpversion(), '7.3', '<')) {
-	die( "Please update PHP to a higher version" );
+	die("Por favor, atualize o PHP para uma versão mais recente");
 }
 
-# Configura o Timezone
-if(!ini_get('date.timezone') ) {
+# Configura o Timezone se não estiver configurado
+if(!ini_get('date.timezone')) {
 	date_default_timezone_set('GMT');
 }
 
-# Preenche a sessão para verificação de segurança
+# Inicializa as variáveis de sessão para controle de acessos
 if (empty($_SESSION['pq_min'])) {
-    $_SESSION['pq_min'] = date('i');
-    $_SESSION['pq_hits'] = 1;
+	$_SESSION['pq_min'] = date('i');
+	$_SESSION['pq_hits'] = 1;
 }
 
-# Limite de 60 acessos por minuto
+# Limita o acesso a 60 requisições por minuto
 if ($_SESSION['pq_hits'] >= 60 && $_SESSION['pq_min'] == date('i')) {
-    header('HTTP/1.1 429 Too Many Requests');
-    header('Retry-After: 60');  // Define o cabeçalho Retry-After para sugerir quando a próxima solicitação deve ser feita
-    die("Too many requests. Please try again later.");
+	header('HTTP/1.1 429 Too Many Requests');
+	header('Retry-After: 60');
+	die("Muitas requisições. Por favor, tente novamente mais tarde.");
 } elseif ($_SESSION['pq_min'] != date('i')) {
-    $_SESSION['pq_hits'] = 0;
-    $_SESSION['pq_min'] = date('i');
+	$_SESSION['pq_hits'] = 0;
+	$_SESSION['pq_min'] = date('i');
 } else {
-    $_SESSION['pq_hits']++;
+	$_SESSION['pq_hits']++;
 }
 
-# Filtro para textos
+/**
+ * Filtra a entrada de dados de acordo com o tipo especificado
+ *
+ * @param string $input Dado de entrada a ser filtrado
+ * @param string $type Tipo de filtragem a ser aplicado
+ * 	- 'html': Filtra a entrada de conteúdo HTML, convertendo caracteres especiais em entidades HTML.
+ * 	- 'url': Filtra a entrada de URL, substituindo espaços por underscores e convertendo caracteres especiais em entidades HTML.
+ * 	- 'user': Filtra a entrada de nome de usuário, convertendo para minúsculas e substituindo espaços por underscores.
+ * 	- 'email': Filtra a entrada de endereço de e-mail, convertendo para minúsculas, substituindo espaços por underscores e validando o formato do e-mail.
+ * 	- 'get': Filtra a entrada do valor recebido via método GET, removendo caracteres especiais.
+ * 	- 'post': Filtra a entrada do valor recebido via método POST, removendo caracteres especiais.
+ * 	- 'pass': Filtra a entrada de senha, adicionando strings e realizando um hash SHA256.
+ * @param string $add Dado adicional para algumas filtragens como 'get', 'post' ou 'pass'
+ * @return mixed Retorna o dado filtrado
+ */
+
 function pq_filter($input, $type = '', $add = '') {
 	$input = trim($input);
-
 	switch ($type) {
 		case 'html':
 			$input = htmlspecialchars($input, ENT_QUOTES | ENT_HTML5, 'UTF-8');
@@ -61,10 +70,6 @@ function pq_filter($input, $type = '', $add = '') {
 		case 'user':
 			$input = strtolower($input);
 			$input = str_replace(" ", "_", $input);
-			break;
-
-		case 'invert':
-			$input = strrev($input);
 			break;
 
 		case 'email':
@@ -91,8 +96,8 @@ function pq_filter($input, $type = '', $add = '') {
 
 		case 'pass':
 			$input = strrev($input);
-			$salt = ($add == '') ? '+ 121 @gS457 dat#yii' : $add ;
-			$input = $salt . '94+ç~b' . pq_key . '212' . $input . $salt;
+			$salt = ($add == '') ? '168584+^@gS457 dat#yii' : $add;
+			$input = $salt . '9+ç~x' . $input . $salt;
 			$input = hash('sha256', $input);
 			$input = substr($input, 0, -2);
 			$input = '$2a$' . $input;
@@ -102,71 +107,100 @@ function pq_filter($input, $type = '', $add = '') {
 			$input = strip_tags($input);
 			break;
 	}
-
 	return $input;
 }
 
-# Data e hora atual
-function pq_time($type = '') {
-	$formats = [
-		'd' => 'd',
-		'm' => 'm',
-		'y' => 'Y',
-		'a' => 'Y',
-		'data' => 'd/m/Y',
-		'date' => 'Y/m/d',
-		'h' => 'h',
-		'm' => 'i',
-		's' => 's',
-		'hora' => 'H:i:s',
-		'hour' => 'H:i:s',
-	];
-
-	return isset($formats[$type]) ? date($formats[$type]) : date('Y/m/d - H:i:s');
-}
-
-# Navegador do usuário
-function pq_browser() {
-	return $_SERVER['HTTP_USER_AGENT'];
-}
-
-# IP do usuário
-function pq_ip() {
-	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-		return $_SERVER['HTTP_CLIENT_IP'];
-	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-		return $_SERVER['HTTP_X_FORWARDED_FOR'];
-	} else {
-		return $_SERVER['REMOTE_ADDR'];
+/**
+ * Retorna informações sobre o cliente, servidor e ambiente de execução, incluindo endereço IP
+ *
+ * @param string $opt Opção para especificar qual informação retornar:
+ * 	- 'ip': Retorna o endereço IP do cliente.
+ * 	- 'browser': Retorna o agente do navegador (user-agent) do cliente.
+ * 	- 'port': Retorna a porta do cliente.
+ * 	- 'referer': Retorna a URL da página de onde o usuário veio.
+ * 	- 'root': Retorna o diretório raiz do documento.
+ * 	- 'host': Retorna o nome do host do servidor.
+ * 	- 'server_addr': Retorna o endereço IP do servidor.
+ * 	- 'server_name': Retorna o nome do servidor.
+ * 	- 'server_software': Retorna o software do servidor.
+ * @return mixed Retorna a informação solicitada (IP, navegador, etc.)
+ */
+function pq_info($opt = 'ip') {
+	switch ($opt) {
+		case 'ip':
+			if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+				return $_SERVER['HTTP_CLIENT_IP'];
+			} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+				return $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+				return $_SERVER['REMOTE_ADDR'];
+			} else {
+				return null;
+			}
+			break;
+		case 'browser':
+			return $_SERVER['HTTP_USER_AGENT'];
+			break;
+		case 'port':
+			return $_SERVER['REMOTE_PORT'];
+			break;
+		case 'referer':
+			return isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null;
+			break;
+		case 'root':
+			return $_SERVER['DOCUMENT_ROOT'];
+			break;
+		case 'host':
+			return $_SERVER['HTTP_HOST'];
+			break;
+		case 'server_addr':
+			return $_SERVER['SERVER_ADDR'];
+			break;
+		case 'server_name':
+			return $_SERVER['SERVER_NAME'];
+			break;
+		case 'server_software':
+			return $_SERVER['SERVER_SOFTWARE'];
+			break;
+		default:
+			return null;
+			break;
 	}
 }
 
-# Envio de e-mail usando Mail
-function pq_mail($email, $subject, $body, $from, $redirect = '', $errorRedirect = '') {
+/**
+ * Envia um email usando a função mail do PHP
+ *
+ * @param string $email Endereço de email de destino
+ * @param string $subject Assunto do email
+ * @param string $body Corpo do email
+ * @param string $from Endereço de email do remetente
+ * @return bool Retorna True se o email for enviado com sucesso ou False caso contrário
+ */
+function pq_mail($email, $subject, $body, $from) {
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 	$headers .= "From: " . $from . "\r\n";
 	$headers .= "Reply-To: " . $from . "\r\n";
 	$headers .= "Return-Path: " . $from . "\r\n";
 	$sendMail = mail($email, $subject, $body, $headers);
-
 	if ($sendMail) {
-		if ($redirect !== '') {
-			header("Location: $redirect");
-		}
+		return true;
 	} else {
-		if ($errorRedirect !== '') {
-			header("Location: $errorRedirect");
-		}
+		return false;
 	}
 }
 
-# Gerador de captcha
-function pq_captcha($name = 'ok', $class = '') {
+/**
+ * Gera um captcha simples com uma operação matemática
+ *
+ * @param string $name Nome do captcha para armazenamento na sessão
+ * @return string Retorna a soma ou subtração gerada do captcha
+ */
+function pq_captcha($name = 'captcha') {
 	$num1 = rand(0, 10);
 	$num2 = rand(0, 10);
 	$operation = (rand(0, 1) == 0) ? '+' : '-';
-
 	if ($operation === '+') {
 		$captchaText = "$num1 + $num2";
 		$captchaResult = $num1 + $num2;
@@ -174,43 +208,57 @@ function pq_captcha($name = 'ok', $class = '') {
 		$captchaText = "$num1 - $num2";
 		$captchaResult = $num1 - $num2;
 	}
-
 	$_SESSION[$name] = $captchaResult;
-
-	$captchaHTML = "<label for='$name'>$captchaText = </label>";
-	$captchaHTML .= "<input type='number' class='$class' name='$name' required>";
-
-	return $captchaHTML;
+	return $captchaText;
 }
 
-# Validador do captcha
-function pq_validCaptcha($value, $name = 'ok') {
+/**
+ * Valida a resposta do captcha fornecida pelo usuário
+ *
+ * @param string $input Resposta do usuário ao captcha
+ * @param string $name Nome do captcha armazenado na sessão
+ * @return bool Retorna True se a resposta estiver correta ou False caso contrário
+ */
+function pq_validCaptcha($input, $name = 'captcha') {
 	if (isset($_SESSION[$name])) {
-		$submittedResult = pq_filter($name);
-		$correctResult = $_SESSION[$name];
+		$submitted = pq_filter($input);
+		$correct = $_SESSION[$name];
 		unset($_SESSION[$name]);
-		return $submittedResult === $correctResult;
+		if ($submitted === $correct) {
+			return true;
+		}
+	}
+	return false;
+}
+
+/**
+ * Realiza o login com uma senha única
+ *
+ * @param string $input Senha fornecida pelo usuário
+ * @param string $correctPass Senha correta para comparação
+ * @param string $token Token para validar sessão
+ * @return bool Retorna True se a senha estiver correta ou False caso contrário
+ */
+function pq_login($input, $correctPass, $token = 'YSB2aWRhIMOpIGN1cnRhIGUgYmVsYQ==') {
+	$inputPass = pq_filter($input, 'post');
+	if ($inputPass === $correctPass) {
+		$_SESSION['pq_login'] = $token;
+		return true;
 	} else {
 		return false;
 	}
 }
 
-# Login com senha única
-function pq_login($input, $pass, $redirect, $errorRedirect = '') {
-	$input = pq_filter($input, 'post');
-	if ($input === $pass) {
-		// Preenche a sessão pq_login
-		$_SESSION['pq_login'] = "YSB2aWRhIMOpIGN1cnRhIGUgYmVsYQ==";
-		header("Location: $redirect");
-	} elseif ($errorRedirect !== '') {
-		header("Location: $errorRedirect");
-	}
-}
-
-# Validação do login
-function pq_validLogin($errorRedirect) {
-	if ($_SESSION['pq_login'] !== "YSB2aWRhIMOpIGN1cnRhIGUgYmVsYQ==" || empty($_SESSION['pq_login'])) {
-		// Redirecionar para a URL especificada em $redirect
-		header("Location: $$errorRedirect");
+/**
+ * Verifica se o usuário está logado
+ *
+ * @param string $token Token para validar sessão
+ * @return bool True se o usuário estiver logado, False caso contrário
+ */
+function pq_validLogin($token = 'YSB2aWRhIMOpIGN1cnRhIGUgYmVsYQ==') {
+	if ($_SESSION['pq_login'] === $token && !empty($_SESSION['pq_login'])) {
+		return true;
+	} else {
+		return false;
 	}
 }
